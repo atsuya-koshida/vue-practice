703820c82301e24683ab3c7c5dce4be3bb961219
atsuya-koshida <koshi0845@gmail.com>
2020-06-05 23:20:17 +0900

    lazy修飾子

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -9,6 +9,12 @@
     <keep-alive>
       <component :is="currentComponent"></component>
     </keep-alive>
+    <div>
+      <h2>イベントのフォーム</h2>
+      <label for="title">タイトル</label>
+      <input id="title" type="text" v-model="eventData.title">
+      <p>{{ eventData.title }}</p>
+    </div>
   </div>
 </template>
 
@@ -21,7 +27,10 @@ export default {
   data() {
     return {
       number: 20,
-      currentComponent: 'Home'
+      currentComponent: 'Home',
+      eventData: {
+        title: ""
+      }
     }
   },
   components: {
@@ -31,9 +40,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-  div {
-    border: 1px solid blue;
-  }
-</style>
