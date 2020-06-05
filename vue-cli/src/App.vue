08eee7a62234a48ea805fb752e97d8c8e833b505
atsuya-koshida <koshi0845@gmail.com>
2020-06-05 23:04:33 +0900

    componentタグで動的コンポーネント

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,27 +1,31 @@
 <template>
   <div>
-    <LikeHeader #default="slotProps">
-      <p>{{ slotProps }}</p>
-      <h2>みなさん</h2>
+    <LikeHeader>
       <h3>初めまして</h3>
-      <p>よろしくお願いします</p>
     </LikeHeader>
     <LikeNumber :total-number="number" @my-click="number = $event"></LikeNumber>
-    <LikeNumber :total-number="number"></LikeNumber>
+    <button @click="currentComponent = 'Home'">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <component :is="currentComponent"></component>
   </div>
 </template>
 
 <script>
-import LikeHeader from "./components/LikeHeader.vue"
+import LikeHeader from "./components/LikeHeader.vue";
+import About from "./components/About.vue";
+import Home from "./components/Home.vue";
 
 export default {
   data() {
     return {
-      number: 20
+      number: 20,
+      currentComponent: 'Home'
     }
   },
   components: {
-    LikeHeader
+    LikeHeader,
+    About,
+    Home,
   }
 }
 </script>
