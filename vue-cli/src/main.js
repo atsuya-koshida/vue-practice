1100b294fea5b36687635693848841c497c5501f
atsuya-koshida <koshi0845@gmail.com>
2020-06-06 10:41:52 +0900

    カスタムディレクティブel, binding

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -4,6 +4,10 @@ import LikeNumber from "./components/LikeNumber.vue"
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber)
+Vue.directive("border", function(el, binding) {
+  el.style.border = "solid black 2px";
+  el.style.borderWidth = binding.value
+});
 
 new Vue({
   render: h => h(App),
