c2270f825b4623ca0063335cce089955858cdd09
atsuya-koshida <koshi0845@gmail.com>
2020-06-07 11:05:52 +0900

    名前付きルート

--- vue-spa/src/views/Home.vue
+++ vue-spa/src/views/Home.vue
@@ -9,7 +9,9 @@
 export default {
   methods: {
     toUsers() {
-      this.$router.push('users')
+      this.$router.push({
+        name: 'users-id-profile', params: { id: 1 }
+      })
     }
   }
 }
